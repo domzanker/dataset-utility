bd7359969c1730b639e26559e8c6b8b565c40d69
Dominic Zanker <mail@dzanker.de>
2020-12-15 14:13:15 +0100

    headless mode

--- dataset_utilities/scripts/create_scene_video.py
+++ dataset_utilities/scripts/create_scene_video.py
@@ -3,6 +3,7 @@ import argparse
 from pathlib import Path
 from tqdm import tqdm, trange
 from concurrent.futures import ProcessPoolExecutor, as_completed
+import pickle
 
 FILES_PATTERN = {"front": "cam_front"}
 
@@ -43,6 +44,35 @@ def write_scene(scene_path, args):
     writer.release()
 
 
+def get_image_collection(scene_path, args):
+    # for every sample_* dir in SCENE_PATH
+    output_path = args.out_path / scene_path.name
+
+    frames = []
+    # print(args.scene_path.glob("sample"))
+    # for sample in trange(len(list(scene_path.glob("sample_*"))), unit="frame"):
+    for sample in range(len(list(scene_path.glob("sample_*")))):
+        sample_path = scene_path / f"sample_{sample}"
+        if not sample_path.is_dir():
+            continue
+
+        out_sample_path = output_path / f"sample_{sample}"
+        out_sample_path.mkdir(parents=True, exist_ok=True)
+        # load every image file
+        for key, perspective in FILES_PATTERN.items():
+            # file = entry.glob(f"{perspective}.png")
+
+            file = sample_path / f"{perspective}.png"
+            img = cv2.imread(str(file))
+            if img is not None:
+                """
+                cv2.imshow("frame", img)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+                """
+                cv2.imwrite(str(out_sample_path / "cam_front.png"), img)
+
+
 def write_all_scenes(args):
     executor = ProcessPoolExecutor(max_workers=10)
     scenes = list(args.path.glob("scene_*"))
@@ -53,9 +83,16 @@ def write_all_scenes(args):
             pbar.update(1)
         """
         with ProcessPoolExecutor(max_workers=8) as executor:
-            futures = {
-                executor.submit(write_scene, scene_path=s, args=args): s for s in scenes
-            }
+            if args.render == 1:
+                futures = {
+                    executor.submit(write_scene, scene_path=s, args=args): s
+                    for s in scenes
+                }
+            else:
+                futures = {
+                    executor.submit(get_image_collection, scene_path=s, args=args): s
+                    for s in scenes
+                }
 
             for future in as_completed(futures):
                 result = future.result()
@@ -69,7 +106,9 @@ if __name__ == "__main__":
     parser.add_argument("--height", type=int, default=1080)
     parser.add_argument("--width", type=int, default=1920)
     parser.add_argument("--preview", type=bool, default=False)
+    parser.add_argument("--render", type=int, default=1)
 
     args = parser.parse_args()
     args.out_path.mkdir(parents=True, exist_ok=True)
+
     write_all_scenes(args)
